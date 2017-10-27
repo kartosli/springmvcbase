39e6e57af1907521973acbd8d443001ba7ebf0e3
snakeyjcf <snakeyjcf@126.com>
2017-10-27 22:45:24 +0800

    更换junit

--- src/main/java/com/n5x/common/base/ResponseCode.java
+++ src/main/java/com/n5x/common/base/ResponseCode.java
@@ -11,23 +11,19 @@ public class ResponseCode {
     /**
      * C开头代码表示系统共用
      */
-    public static String Common_Success = "C000";  //处理成功
+    public static final String Common_Success = "C000";  //处理成功
 
-    public static String Common_Fail = "C500"; //处理失败
+    public static final String Common_Fail = "C500"; //处理失败
 
-    public static String Common_Error = "C505"; //系统报错
+    public static final String Common_Error = "C505"; //系统报错
 
-    public static String Common_NotLogIn = "C501"; //未登陆
+    public static final String Common_NotLogIn = "C501"; //未登陆
 
-    public static String Common_NoSchoolAndGrade = "C502"; //未设置学校和年级
+    public static final String Common_ParamError = "C504"; //参数错误
 
-    public static String Common_NoVersion = "C503"; //未设置教材版本
+    public static final String Common_NoAccess = "C506"; //无权限
 
-    public static String Common_ParamError = "C504"; //参数错误
-
-    public static String Common_NoAccess = "C506"; //无权限
-
-    public static String Common_DataNotFound = "C404"; //无数据
+    public static final String Common_DataNotFound = "C404"; //无数据
 
     /**
      * S开头代码表示接口自定义(此处不作定义,由各个接口独立定义)
@@ -43,10 +39,6 @@ public class ResponseCode {
                 return ResponseMsg.Common_Error;
             else if (Common_NotLogIn.equals(code))
                 return ResponseMsg.Common_NotLogIn;
-            else if (Common_NoSchoolAndGrade.equals(code))
-                return ResponseMsg.Common_NoSchoolAndGrade;
-            else if (Common_NoVersion.equals(code))
-                return ResponseMsg.Common_NoVersion;
             else if (Common_ParamError.equals(code))
                 return ResponseMsg.Common_ParamError;
             else if (Common_DataNotFound.equals(code))
